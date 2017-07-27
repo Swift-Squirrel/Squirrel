4a36af5cbe3e2d4f1a364090383eefc6e34dce39
Filip Klembara <filip@klembara.pro>
2017-07-27 14:34:59 +0200

    move route to ResponseManager

--- Sources/ResponseManager.swift
+++ Sources/ResponseManager.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Reflection
 
 class ResponseManager {
 
@@ -17,7 +18,44 @@ class ResponseManager {
     private init() {
     }
 
-    func route(method: HTTPHeaders.Method, url: String, handler: @escaping AnyResponseHandler) {
+    public func route(get url: String, handler: @escaping (Request) throws -> Any) {
+        route(method: .get, url: url, handler: handler)
+    }
+
+    public func route(get url: String, handler: @escaping () throws -> Any) {
+        route(get: url) { (_ :Request) in
+            return try handler()
+        }
+    }
+
+    public func route<T>(get url: String, handler: @escaping (Request, T) throws -> Any) {
+        let closure: AnyResponseHandler = {
+            (req: Request) in
+            let blueprint = Blueprint(of: T.self)
+            let values = req.getURLParameters()
+            guard let converted = blueprint.construct(using: values) else {
+                throw HTTPError(status: .badRequest, description: "Wrong parameters type or missing parameters")
+            }
+            return try handler(req, converted)
+        }
+        route(get: url, handler: closure)
+    }
+
+    public func route<T>(get url: String, handler: @escaping (T) throws -> Any) {
+        let closure: AnyResponseHandler = {
+            (req: Request) in
+            let blueprint = Blueprint(of: T.self)
+            let values = req.getURLParameters()
+            guard let converted = blueprint.construct(using: values) else {
+                throw HTTPError(status: .badRequest, description: "Wrong parameters type or missing parameters")
+            }
+            return try handler(converted)
+        }
+        route(get: url, handler: closure)
+    }
+
+
+    private func route(method: HTTPHeaders.Method, url: String, handler: @escaping AnyResponseHandler) {
         routeTree.add(route: url, forMethod: method, handler: handler)
     }
 
--- Sources/Server+Routing.swift
+++ Sources/Server+Routing.swift
@@ -10,40 +10,19 @@ import Foundation
 import Reflection
 
 extension Server {
-
     public func route(get url: String, handler: @escaping (Request) throws -> Any) {
-        responseManager.route(method: .get, url: url, handler: handler)
+        responseManager.route(get: url, handler: handler)
     }
 
     public func route(get url: String, handler: @escaping () throws -> Any) {
-        route(get: url) { (_ :Request) in
-            return try handler()
-        }
+        responseManager.route(get: url, handler: handler)
     }
 
     public func route<T>(get url: String, handler: @escaping (Request, T) throws -> Any) {
-        let closure: AnyResponseHandler = {
-            (req: Request) in
-            let blueprint = Blueprint(of: T.self)
-            let values = req.getURLParameters()
-            guard let converted = blueprint.construct(using: values) else {
-                throw HTTPError(status: .badRequest, description: "Wrong parameters type or missing parameters")
-            }
-            return try handler(req, converted)
-        }
-        route(get: url, handler: closure)
+        responseManager.route(get: url, handler: handler)
     }
 
     public func route<T>(get url: String, handler: @escaping (T) throws -> Any) {
-        let closure: AnyResponseHandler = {
-            (req: Request) in
-            let blueprint = Blueprint(of: T.self)
-            let values = req.getURLParameters()
-            guard let converted = blueprint.construct(using: values) else {
-                throw HTTPError(status: .badRequest, description: "Wrong parameters type or missing parameters")
-            }
-            return try handler(converted)
-        }
-        route(get: url, handler: closure)
+        responseManager.route(get: url, handler: handler)
     }
 }
