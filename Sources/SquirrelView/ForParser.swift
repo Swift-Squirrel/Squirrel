ea6767ce68a0a5fada5d6a20675d51f0c3a18fc8
Filip Klembara <filip@klembara.pro>
2017-08-06 21:40:51 +0200

    for (key, value) in arr

--- Sources/SquirrelView/ForParser.swift
+++ Sources/SquirrelView/ForParser.swift
@@ -47,40 +47,33 @@ struct ForParser: ParseCommandProtocol {
             keyName = pom
         }
 
-
-
         var res = ""
+        let prevValue = ViewParser.data[variableName]
+        var pomText = text
         if let keyName = keyName {
-            let itms = ViewParser.get(name: source) as? [Any]
             guard let items = ViewParser.get(name: source) as? [String: Any] else {
                 throw ParseError(kind: .unknownVariable(name: source), description: "\(source) is unknown variable or is not array")
             }
-            let prevValue = ViewParser.data[variableName]
             let prevKey = ViewParser.data[keyName]
-            var pomText = text
             for (key, value) in items {
                 pomText = text
                 ViewParser.data[keyName] = key
                 ViewParser.data[variableName] = value
                 res += try ViewParser.parse(text: &pomText, prevChar: &prevChar, partial: true)
             }
-            text = pomText
             ViewParser.data[keyName] = prevKey
-            ViewParser.data[variableName] = prevValue
         } else {
             guard let items = ViewParser.get(name: source) as? [Any] else {
                 throw ParseError(kind: .unknownVariable(name: source), description: "\(source) is unknown variable or is not array")
             }
-            let prevValue = ViewParser.data[variableName]
-            var pomText = text
             for item in items {
                 pomText = text
                 ViewParser.data[variableName] = item
                 res += try ViewParser.parse(text: &pomText, prevChar: &prevChar, partial: true)
             }
-            text = pomText
-            ViewParser.data[variableName] = prevValue
         }
+        text = pomText
+        ViewParser.data[variableName] = prevValue
 
         return res
     }
