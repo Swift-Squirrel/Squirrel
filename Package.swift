888e8b275d3519fda53910756605290378f243dc
Filip Klembara <filip@klembara.pro>
2017-08-30 00:20:10 +0200

    Linuxlike SwiftyJSON

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git",  from: "1.4.0"),
         .package(url: "https://github.com/behrang/YamlSwift.git",  from: "3.4.0"),
         .package(url: "https://github.com/LeoNavel/Evaluation.git",  from: "0.2.0"),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git",  from: "3.1.4"),
+	.package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", from: "15.0.1"),
         .package(url: "https://github.com/sharplet/Regex.git",  from: "1.1.0"),
         .package(url: "https://github.com/IBM-Swift/BlueSocket", from: "0.12.61"),
         .package(url: "https://github.com/tris-foundation/test.git", from: "0.4.3")
