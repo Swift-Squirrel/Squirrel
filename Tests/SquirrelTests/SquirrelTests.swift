8ad670e9b86334a02b1d2cb1c28c69ac3e775030
Filip Klembara <filip@klembara.pro>
2017-09-05 00:27:47 +0200

    remove wrong test

--- Tests/SquirrelTests/SquirrelTests.swift
+++ Tests/SquirrelTests/SquirrelTests.swift
@@ -2,13 +2,6 @@ import XCTest
 @testable import Squirrel
 
 class SquirrelTests: XCTestCase {
-    
-    func testServer() {
-        let server: Any = Server(port: 8080, serverRoot: "/Users/Navel/Leo/Skola/3BIT/IBT/Micros")
-        XCTAssertTrue(server is Server)
-    }
-
     static var allTests = [
-        ("testServer", testServer)
     ]
 }
