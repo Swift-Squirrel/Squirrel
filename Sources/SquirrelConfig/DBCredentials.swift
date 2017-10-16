adec6a5bd934e4a530523d24a88773504e711416
Filip Klembara <filip@klembara.pro>
2017-10-16 21:46:33 +0200

    remove squirrel connector

--- Sources/SquirrelConfig/DBCredentials.swift
+++ Sources/SquirrelConfig/DBCredentials.swift
@@ -5,17 +5,17 @@
 //  Created by Filip Klembara on 8/23/17.
 //
 
-struct DBCredentials: CustomStringConvertible {
-    struct UserCredentails {
+public struct DBCredentials: CustomStringConvertible {
+    public struct UserCredentails {
         let username: String
         let password: String
     }
-    let user: UserCredentails?
-    let dbname: String
-    let host: String
-    let port: Int
+    public let user: UserCredentails?
+    public let dbname: String
+    public let host: String
+    public let port: Int
 
-    var description: String {
+    public var description: String {
         if let user = self.user {
             let pw = user.password.map({_ in return "*" })
             return "mongodb://\(user.username):\(pw)@\(host):\(port)/\(dbname)"
